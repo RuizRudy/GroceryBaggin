1e2df2c50428365ed2133920b620fbe8b2fc10f3
Rudy Ruiz <rudyruiz@u.boisestate.edu>
2019-10-28 00:10:23 -0600

    Update bagit.sh

--- groceryBaggin/src/bagit.sh
+++ groceryBaggin/src/bagit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ $# -eq 0 ]
     then
-	echo "Usage: ./bagit.sh 'filename.txt' [-breadth/depth]"
+	echo "Usage: ./bagit.sh 'filename.txt' [-slow/minstdv/local]"
 	exit 0
 fi
 javac GroceryBaggin.java Bag.java Item.java WorldState.java
